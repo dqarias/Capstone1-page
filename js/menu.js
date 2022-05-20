4a8da741d88398a1a66f2993c4f3ddb4b9fc51b0
dqarias <dino.quispea@gmail.com>
2022-05-20 08:35:19 -0500

    Fix linter errors

--- js/menu.js
+++ js/menu.js
@@ -2,12 +2,12 @@ const hamMenu = document.querySelector('.navigation__menu');
 const navMenu = document.querySelector('.navigation__links');
 
 hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 const navLink = document.querySelectorAll('.navigation__link');
-  navLink.forEach((e) => e.addEventListener('click', () => {
-    hamMenu.classList.remove('active');
-    navMenu.classList.remove('active');
-  }));
+navLink.forEach((e) => e.addEventListener('click', () => {
+  hamMenu.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
--- js/programs.js
+++ js/programs.js
@@ -1,55 +1,55 @@
 const programsData = [
-    {
-      id: 0,
-      name: 'Sponsor an andean cat',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/sponsor2.jpg',
-    },
-    {
-      id: 1,
-      name: 'Adopt a symbolic cat',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/adopt.jpg',
-    },
-    {
-      id: 2,
-      name: 'Educa Cat',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/education.jpg',
-    },
-    {
-      id: 3,
-      name: 'Catcrafts',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/catcrafts.jpg',
-    },
-    {
-      id: 4,
-      name: 'Cat Gold',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/mining.png',
-    },
-    {
-      id: 5,
-      name: 'In the field',
-      phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
-      image: './assets/images/field.jpeg',
-    },
-  ];
+  {
+    id: 0,
+    name: 'Sponsor an andean cat',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/sponsor2.jpg',
+  },
+  {
+    id: 1,
+    name: 'Adopt a symbolic cat',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/adopt.jpg',
+  },
+  {
+    id: 2,
+    name: 'Educa Cat',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/education.jpg',
+  },
+  {
+    id: 3,
+    name: 'Catcrafts',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/catcrafts.jpg',
+  },
+  {
+    id: 4,
+    name: 'Cat Gold',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/mining.png',
+  },
+  {
+    id: 5,
+    name: 'In the field',
+    phrase: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, eveniet ad! Ullam dignissimos voluptatum voluptatibus?',
+    image: './assets/images/field.jpeg',
+  },
+];
 
 const programs = document.querySelector('.program');
 
 const renderProgram = () => {
-    programsData.forEach((program) => {
-        const programContainer = document.createElement('article');
-        programContainer.classList.add('program__container');
-        programContainer.innerHTML = `
+  programsData.forEach((program) => {
+    const programContainer = document.createElement('article');
+    programContainer.classList.add('program__container');
+    programContainer.innerHTML = `
         <div class="programs__image">
           <img
             class="program-img"
@@ -67,8 +67,8 @@ const renderProgram = () => {
           </p>
         </div>
         `;
-        programs.appendChild(programContainer);
-    });
+    programs.appendChild(programContainer);
+  });
 };
 
 renderProgram();
